683df36ca8c6c9eb60272226c356b54cd612e756
inboundwebhub <dev.abhicenation@gmail.com>
2021-11-26 12:43:46 +0530

    Shripati's New Commit. Added Favicon and Public folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use('/public',express.static(__dirname+'/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 let routeIndex = require('./Routers/RouteIndex');
